5db355f07ea9c6d39ffd348377281db60b1575f2
MythosOne <107143884+MythosOne@users.noreply.github.com>
2024-06-30 15:07:54 +0300

    Fixing styled Logo

--- src/Components/Logo/Logo.styled.js
+++ src/Components/Logo/Logo.styled.js
@@ -23,21 +23,27 @@ export const Block = styled.div`
   align-items: center;
   padding: 0 5px;
   background-color: #2196f3;
-  margin-top: 5px;
   height: 24px;
   /* margin-top: 2px; */
 
   /* @media screen and (min-width: 768px) {
     height: 18px;
   } */
+
+     @media screen and (min-width: 1279px) {
+      margin-top: 5px;
+  }
 `;
 
 export const PortfolioText = styled.span`
-  margin-bottom: 4px;
   color: #fff;
   font-size: 34px;
 
   /* @media screen and (min-width: 768px) {
     font-size: 24px;
   } */
+
+    @media screen and (min-width: 1279px) {
+      margin-bottom: 4px;
+  }
 `;
